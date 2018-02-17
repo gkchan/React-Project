95d1f68b8f0046609d5963e72492497a9baf57fb
Grace <gkchan09@gmail.com>
2018-02-17 01:23:18 +0000

    Change number on button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,7 +16,7 @@ class Hello extends React.Component {
                 <h1>Hello World {this.state.variable}</h1>
                 <h3>{this.state.number}</h3>
                 <button className="hello" onClick={() => this.setState({number: "1"})}>
-                Button</button>
+                Button: {this.state.number}</button>
             </div>);
     }
 }
