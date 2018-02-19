0bf19e3f7f48971f851edd0b5f7a974ade18517d
Grace <gkchan09@gmail.com>
2018-02-19 04:21:34 +0000

    Show sum of numbers

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,8 +8,8 @@ class Hello extends React.Component {
         super();
         this.state = {
             variable: "Viewer",
-            number: "0",
-            number2: "4",
+            number: 0,
+            number2: 4,
         };
     }
     render() {
@@ -20,10 +20,11 @@ class Hello extends React.Component {
                     <li>{this.state.number}</li>
                     <li>{this.state.number2}</li>
                     <li>{this.state.number} + {this.state.number2}</li>
+                    <li>{this.state.number + this.state.number2}</li>
                 </ul>
-                <button className="hello" onClick={() => this.setState({number: "1"})}>
+                <button className="hello" onClick={() => this.setState({number: 1})}>
                 Change number1: {this.state.number}</button>
-                <button className="hello" onClick={() => this.setState({number2: "3"})}>
+                <button className="hello" onClick={() => this.setState({number2: 3})}>
                 Change number2: {this.state.number2}</button>
             </div>);
     }
