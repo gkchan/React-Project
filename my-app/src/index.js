fefaee2ad509c8e655d3510293738f2a10162ae6
Grace <gkchan09@gmail.com>
2018-02-22 03:07:55 +0000

    Fix bug with changing number and add descriptions

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,13 +17,13 @@ class Hello extends React.Component {
         return (<div>
                 <h1>Hello World {this.state.variable}</h1>
                 <ul>
-                    <li>{this.state.number1}</li>
-                    <li>{this.state.number2}</li>
-                    <li>{this.state.number1} + {this.state.number2}</li>
-                    <li>{this.state.number1 + this.state.number2}</li>
+                    <li>First Number: {this.state.number1}</li>
+                    <li>Second Number: {this.state.number2}</li>
+                    <li>Add: {this.state.number1} + {this.state.number2}</li>
+                    <li>Sum: {this.state.number1 + this.state.number2}</li>
                     <li>Random Number: {Math.random()}</li>
                 </ul>
-                <button className="hello" onClick={() => this.setState({number: this.state.number1 + 1})}>
+                <button className="hello" onClick={() => this.setState({number1: this.state.number1 + 1})}>
                 Add 1 to number1: {this.state.number1}</button>
                 <button className="hello" onClick={() => this.setState({number2: 3})}>
                 Change number2: {this.state.number2}</button>
