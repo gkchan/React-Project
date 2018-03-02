bdb8276a5873186cfd0f56cb96239759b8da6752
Grace <gkchan09@gmail.com>
2018-03-02 04:51:44 +0000

    Adjust indent

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,20 +15,20 @@ class Hello extends React.Component {
     render() {
 
         return (<div>
-                <h1>Hello World {this.state.variable}</h1>
-                <ul>
-                    <li>First Number: {this.state.number1}</li>
-                    <li>Second Number: {this.state.number2}</li>
-                    <li>Add: {this.state.number1} + {this.state.number2}</li>
-                    <li>Sum: {this.state.number1 + this.state.number2}</li>
-                    <li>Random Number: {Math.random()}</li>
-                </ul>
-                <button className="hello" onClick={() => this.setState({number1: this.state.number1 + 1})}>
-                Add 1 to number1: {this.state.number1}</button>
-                &nbsp;
-                <button className="hello" onClick={() => this.setState({number2: this.state.number2 - 1})}>
-                Subtract 1 from number2: {this.state.number2}</button>
-            </div>);
+                    <h1>Hello World {this.state.variable}</h1>
+                    <ul>
+                        <li>First Number: {this.state.number1}</li>
+                        <li>Second Number: {this.state.number2}</li>
+                        <li>Add: {this.state.number1} + {this.state.number2}</li>
+                        <li>Sum: {this.state.number1 + this.state.number2}</li>
+                        <li>Random Number: {Math.random()}</li>
+                    </ul>
+                    <button className="hello" onClick={() => this.setState({number1: this.state.number1 + 1})}>
+                    Add 1 to number1: {this.state.number1}</button>
+                    &nbsp;
+                    <button className="hello" onClick={() => this.setState({number2: this.state.number2 - 1})}>
+                    Subtract 1 from number2: {this.state.number2}</button>
+                </div>);
     }
 }
 
