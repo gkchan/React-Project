b5c5df49a9fe8c654a5641b754c77ed636d734ad
Grace <gkchan09@gmail.com>
2018-03-17 03:18:28 +0000

    Add greeting when clicking button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,6 +8,7 @@ class Hello extends React.Component {
         super();
         this.state = {
             variable: "Viewer",
+            greeting: "",
             number1: 0,
             number2: 4,
         };
@@ -16,6 +17,7 @@ class Hello extends React.Component {
 
         return (<div>
                     <h1>Hello World {this.state.variable}</h1>
+                    <h2>{this.state.greeting}</h2>
                     <ul>
                         <li>First Number: {this.state.number1}</li>
                         <li>Second Number: {this.state.number2}</li>
@@ -23,7 +25,7 @@ class Hello extends React.Component {
                         <li>Sum: {this.state.number1 + this.state.number2}</li><br/>
                         <li id="random">Random Number: {Math.random()}</li>
                     </ul>
-                    <button className="hello" onClick={() => this.setState({number1: this.state.number1 + 1})}>
+                    <button className="hello" onClick={() => this.setState({number1: this.state.number1 + 1, greeting: "Welcome!"})}>
                     Add 1 to number1: {this.state.number1}</button>
                     &nbsp;
                     <button className="hello" onClick={() => this.setState({number2: this.state.number2 - 1})}>
